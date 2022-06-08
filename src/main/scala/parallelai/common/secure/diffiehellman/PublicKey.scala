ee87706e273ef437ca721ade1aade5dfd718d994
Chandan Rajah <chandan.rajah@gmail.com>
2022-06-08 02:45:54 +0000

    Spray JSON formats for public keys - refactor

--- src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
+++ src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
@@ -2,16 +2,17 @@ package parallelai.common.secure.diffiehellman
 
 import java.security.KeyPair
 import java.util.Base64.{ getDecoder, getEncoder }
+import spray.json.DefaultJsonProtocol._
 import spray.json.{ JsString, JsValue, RootJsonFormat, deserializationError }
 import org.apache.commons.lang3.SerializationUtils._
 
-trait PublicKey {
+sealed trait PublicKey {
   def value: Array[Byte]
 
   def keyPair: KeyPair
 }
 
-trait PublicKeyImplicits {
+trait PublicKeyJsonFormat {
   implicit val keyPairRootJsonFormat: RootJsonFormat[KeyPair] = new RootJsonFormat[KeyPair] {
     def read(json: JsValue): KeyPair = json match {
       case JsString(j) => deserialize(getDecoder decode j).asInstanceOf[KeyPair]
@@ -22,3 +23,19 @@ trait PublicKeyImplicits {
       new JsString(getEncoder encodeToString serialize(keyPair))
   }
 }
+
+case class ClientPublicKey(value: Array[Byte], keyPair: KeyPair) extends PublicKey
+
+object ClientPublicKey extends PublicKeyJsonFormat {
+  implicit val clientPublicKeyRootJsonFormat: RootJsonFormat[ClientPublicKey] =
+    jsonFormat2(ClientPublicKey.apply)
+}
+
+case class ServerPublicKey(value: Array[Byte], keyPair: KeyPair) extends PublicKey
+
+object ServerPublicKey extends PublicKeyJsonFormat {
+  implicit val serverPublicKeyRootJsonFormat: RootJsonFormat[ServerPublicKey] =
+    jsonFormat2(ServerPublicKey.apply)
+}
+
+case class ServerSharedSecret(value: Array[Byte])
--- src/main/scala/parallelai/common/secure/diffiehellman/ServerSharedSecret.scala
+++
@@ -1,3 +0,0 @@
-package parallelai.common.secure.diffiehellman
-
-case class ServerSharedSecret(value: Array[Byte])
