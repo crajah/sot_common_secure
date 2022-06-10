31caa48c31814b0fc4b3a886fe00f7b9a5617441
Chandan Rajah <chandan.rajah@gmail.com>
2022-06-10 02:43:43 +0000

    Circe encoders and decoders

--- src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
+++ src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
@@ -1,5 +1,7 @@
 package parallelai.common.secure.diffiehellman
 
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
@@ -12,6 +14,10 @@ case class ClientPublicKey(value: Array[Byte]) extends PublicKey
 object ClientPublicKey {
   implicit val clientPublicKeyRootJsonFormat: RootJsonFormat[ClientPublicKey] =
     jsonFormat1(ClientPublicKey.apply)
+
+  implicit val encoder: Encoder[ClientPublicKey] = deriveEncoder
+
+  implicit val decoder: Decoder[ClientPublicKey] = deriveDecoder
 }
 
 case class ServerPublicKey(value: Array[Byte]) extends PublicKey
@@ -19,4 +25,8 @@ case class ServerPublicKey(value: Array[Byte]) extends PublicKey
 object ServerPublicKey {
   implicit val serverPublicKeyRootJsonFormat: RootJsonFormat[ServerPublicKey] =
     jsonFormat1(ServerPublicKey.apply)
+
+  implicit val encoder: Encoder[ServerPublicKey] = deriveEncoder
+
+  implicit val decoder: Decoder[ServerPublicKey] = deriveDecoder
 }
