53acf4e1db1385ea1ef80efbed509fe39cd7600c
Chandan Rajah <chandan.rajah@gmail.com>
2022-06-13 21:36:48 +0000

    Encrypted parameterized with typeclasses

--- src/main/scala/parallelai/common/secure/Encrypted.scala
+++ src/main/scala/parallelai/common/secure/Encrypted.scala
@@ -1,73 +1,36 @@
 package parallelai.common.secure
 
+import cats.implicits._
 import io.circe.Decoder.Result
 import io.circe._
-import io.circe.syntax._
-import spray.json.DefaultJsonProtocol._
-import spray.json._
-import org.apache.commons.lang3.SerializationUtils.{deserialize, serialize}
 
-case class Encrypted private (private val value: Array[Byte], private val params: Option[Array[Byte]]) {
-  def decrypt(implicit crypto: CryptoMechanic): Array[Byte] = Encrypted decrypt this
-
-  def decryptT[T: FromBytes](implicit crypto: CryptoMechanic): T = Encrypted decryptT this
+case class Encrypted[T: ToBytes: FromBytes] private (private val value: Array[Byte], private val params: Option[Array[Byte]]) {
+  def decrypt(implicit crypto: CryptoMechanic): T = Encrypted decrypt this
 }
 
 object Encrypted {
-  implicit val rootJsonFormat: RootJsonFormat[Encrypted] = jsonFormat2(Encrypted.apply)
+  // TODO
+  // implicit def rootJsonFormat[T: ToBytes: FromBytes]: RootJsonFormat[Encrypted[T]] = jsonFormat2(Encrypted.apply)
 
-  implicit val encoder: Encoder[Encrypted] =
+  implicit def encoder[T: Encoder]: Encoder[Encrypted[T]] =
     Encoder.forProduct2("value", "params")(e => (e.value, e.params))
 
-  implicit val decoder: Decoder[Encrypted] =
-    Decoder.forProduct2("value", "params")(Encrypted.apply)
+  // TODO - Sort out mess
+  implicit def decoder[T: Decoder: ToBytes: FromBytes]: Decoder[Encrypted[T]] = new Decoder[Encrypted[T]] {
+    def apply(c: HCursor): Result[Encrypted[T]] = {
+      val value = c.top.get.asObject.get("value").get.as[Array[Byte]].right.get
+      val params = c.top.get.asObject.get("params").fold(none[Array[Byte]]) { _.as[Array[Byte]].right.get.some }
 
-  def apply[T: ToBytes](value: T)(implicit crypto: CryptoMechanic): Encrypted = {
+      Right(Encrypted[T](value, params))
+    }
+  }
+
+  def apply[T: ToBytes: FromBytes](value: T)(implicit crypto: CryptoMechanic): Encrypted[T] = {
     val CryptoResult(cryptoPayload, cryptoParams) = crypto.encrypt(ToBytes[T].apply(value))
 
     new Encrypted(cryptoPayload.repr, cryptoParams)
   }
 
-  def decrypt(encrypted: Encrypted)(implicit crypto: CryptoMechanic): Array[Byte] =
-    crypto.decrypt(encrypted.value, encrypted.params).payload.repr
-
-  def decryptT[T: FromBytes](encrypted: Encrypted)(implicit crypto: CryptoMechanic): T =
+  def decrypt[T: FromBytes](encrypted: Encrypted[T])(implicit crypto: CryptoMechanic): T =
     FromBytes[T].apply(crypto.decrypt(encrypted.value, encrypted.params).payload.repr)
-}
-
-case class Encrypteds(values: Map[String, Encrypted]) {
-  def apply(key: String): Encrypted = values(key)
-
-  def get(key: String): Option[Encrypted] = values get key
-}
-
-object Encrypteds {
-  implicit val encryptedsToBytes: ToBytes[Encrypteds] = new ToBytes[Encrypteds] {
-    def apply(encrypteds: Encrypteds): Array[Byte] = serialize(encrypteds)
-  }
-
-  implicit val encryptedsFromBytes: FromBytes[Encrypteds] = new FromBytes[Encrypteds] {
-    def apply(as: Array[Byte]): Encrypteds = deserialize(as).asInstanceOf[Encrypteds]
-  }
-
-  implicit val rootJsonFormat: RootJsonFormat[Encrypteds] = new RootJsonFormat[Encrypteds] {
-    def write(encrypteds: Encrypteds): JsValue = encrypteds.values.toJson
-
-    def read(json: JsValue): Encrypteds = Encrypteds(json.asJsObject.fields.map { case (k, v) =>
-      k -> v.convertTo[Encrypted]
-    })
-  }
-
-  implicit val encoder: Encoder[Encrypteds] = new Encoder[Encrypteds] {
-    def apply(encrypteds: Encrypteds): Json = encrypteds.values.asJson
-  }
-
-  implicit val decoder: Decoder[Encrypteds] = new Decoder[Encrypteds] {
-    def apply(c: HCursor): Result[Encrypteds] =
-      Right(Encrypteds(c.value.asObject.get.toMap.map { case (k, j) =>
-        k -> j.as[Encrypted].right.get
-      }))
-  }
-
-  def apply(values: (String, Encrypted)*) = new Encrypteds(values.toMap)
 }
--- src/test/scala/parallelai/common/secure/EncryptedSpec.scala
+++ src/test/scala/parallelai/common/secure/EncryptedSpec.scala
@@ -1,7 +1,8 @@
 package parallelai.common.secure
 
+import io.circe.Json
+import io.circe.parser._
 import io.circe.syntax._
-import spray.json._
 import org.scalatest.{Inside, MustMatchers, WordSpec}
 
 class EncryptedSpec extends WordSpec with MustMatchers with Inside {
@@ -11,109 +12,47 @@ class EncryptedSpec extends WordSpec with MustMatchers with Inside {
     "encrypt and decrypt given some Crypto mechanism" in {
       val message = "Hello world"
 
-      val encryptedMessage = Encrypted(message)
+      val encryptedMessage = Encrypted[String](message)
       val decryptedMessage = encryptedMessage.decrypt
 
-      new String(decryptedMessage) mustEqual message
-    }
-
-    "encrypt and convert to JSON which is decrypted given some Crypto mechanism" in {
-      val message = "Hello world"
-
-      val encryptedMessage = Encrypted(message)
-      val encryptedJson: JsValue = encryptedMessage.toJson
-
-      val decryptedMessage = encryptedJson.convertTo[Encrypted].decrypt
-
-      new String(decryptedMessage) mustEqual message
-    }
-
-    "encrypt and convert to String which is decrypted given some Crypto mechanism" in {
-      val message = "Hello world"
-
-      val encryptedMessage = Encrypted(message)
-      val encryptedJson: JsValue = JsObject("productToken" -> encryptedMessage.toJson)
-      val encryptedString = encryptedJson.prettyPrint
-
-      val encryptedBytes = encryptedString.parseJson.asJsObject().fields("productToken").convertTo[Encrypted]
-      val decryptedMessage = encryptedBytes.decrypt
-
-      new String(decryptedMessage) mustEqual message
-    }
-
-    "encrypt and convert to String which is decrypted back to original type given some Crypto mechanism" in {
-      val message = "Hello world"
-
-      val encryptedMessage = Encrypted(message)
-      val encryptedJson: JsValue = encryptedMessage.toJson
-      val encryptedString = encryptedJson.compactPrint
-
-      val encryptedBytes = encryptedString.parseJson.convertTo[Encrypted]
-      val decryptedMessage = encryptedBytes.decryptT[String]
-
       decryptedMessage mustEqual message
-    }
-
-    "converted to Spray JSON" in {
-      val message = "Hello world"
-      val encryptedMessage = Encrypted(message)
-
-      encryptedMessage.toJson.convertTo[Encrypted].decryptT[String] mustEqual message
     }
 
     "converted to Circe JSON" in {
       val message = "Hello world"
       val encryptedMessage = Encrypted(message)
 
-      encryptedMessage.asJson.as[Encrypted].right.get.decryptT[String] mustEqual message
-    }
-  }
-
-  "Encrypteds" should {
-    "have each entry encrypted and decrypted as a Map of Encrypted" in {
-      val message1 = "Hello world"
-      val message2 = 99
-
-      val encryptedMessages = Encrypteds("message1" -> Encrypted(message1), "message2" -> Encrypted(message2))
-      encryptedMessages("message1").decryptT[String] mustEqual message1
-
-      inside(encryptedMessages.get("message2")) {
-        case Some(encrypted) => encrypted.decryptT[Int] mustEqual message2
-      }
+      encryptedMessage.asJson.as[Encrypted[String]].right.get.decrypt mustEqual message
     }
 
-    "be converted to and from Spray JSON" in {
-      val message1 = "Hello world"
-      val message2 = 99
+    // TODO
+    /*"converted to Spray JSON" in {
+      val message = "Hello world"
+      val encryptedMessage = Encrypted(message)
 
-      val encrypteds = Encrypteds("message1" -> Encrypted(message1), "message2" -> Encrypted(message2))
-      val encryptedsJson = encrypteds.toJson
+      encryptedMessage.toJson.convertTo[Encrypted].decryptT[String] mustEqual message
+    }*/
 
-      encryptedsJson.asJsObject.fields must (contain key "message1" and contain key "message2")
-      encryptedsJson.convertTo[Encrypteds].values must (contain key "message1" and contain key "message2")
+    "encrypt and convert to JSON which is decrypted given some Crypto mechanism" in {
+      val message = "Hello world"
+
+      val encryptedMessage = Encrypted(message)
+      val encryptedJson: Json = encryptedMessage.asJson
+
+      val decryptedMessage = encryptedJson.as[Encrypted[String]].right.get.decrypt
+
+      decryptedMessage mustEqual message
     }
 
-    "be converted to and from Circe JSON" in {
-      val message1 = "Hello world"
-      val message2 = 99
+    "encrypt and convert to String which is decrypted given some Crypto mechanism" in {
+      val message = "Hello world"
 
-      val encrypteds = Encrypteds("message1" -> Encrypted(message1), "message2" -> Encrypted(message2))
-      val encryptedsJson = encrypteds.asJson
+      val encryptedMessage = Encrypted(message)
+      val Right(encryptedString) = parse(encryptedMessage.asJson.spaces4).right.get.as[Encrypted[String]]
 
-      encryptedsJson.as[Encrypteds].right.get.values must (contain key "message1" and contain key "message2")
-    }
+      val decryptedMessage = encryptedString.decrypt
 
-    "be converted to and from bytes" in {
-      def toBytes[T: ToBytes](value: T): Array[Byte] = ToBytes[T].apply(value)
-
-      val message1 = "Hello world"
-      val message2 = 99
-
-      val encrypteds = Encrypteds("message1" -> Encrypted(message1), "message2" -> Encrypted(message2))
-      val serialized = toBytes(encrypteds)
-      val encryptedsDeserialized = FromBytes[Encrypteds].apply(serialized)
-
-      encryptedsDeserialized.values must (contain key "message1" and contain key "message2")
+      decryptedMessage mustEqual message
     }
   }
 }
