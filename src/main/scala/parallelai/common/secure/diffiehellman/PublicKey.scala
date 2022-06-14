9e6305b0912fd16e5137117efd6dfe5641a9fbce
Chandan Rajah <chandan.rajah@gmail.com>
2022-06-14 01:06:02 +0000

    Serialization/deserialization of public keys

--- src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
+++ src/main/scala/parallelai/common/secure/diffiehellman/PublicKey.scala
@@ -1,9 +1,11 @@
 package parallelai.common.secure.diffiehellman
 
-import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
-import io.circe.{ Decoder, Encoder }
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
+import org.apache.commons.lang3.SerializationUtils.{deserialize, serialize}
+import parallelai.common.secure.{FromBytes, ToBytes}
 
 sealed trait PublicKey {
   def value: Array[Byte]
@@ -18,6 +20,14 @@ object ClientPublicKey {
   implicit val encoder: Encoder[ClientPublicKey] = deriveEncoder
 
   implicit val decoder: Decoder[ClientPublicKey] = deriveDecoder
+
+  implicit val toBytes: ToBytes[ClientPublicKey] = new ToBytes[ClientPublicKey] {
+    def apply(clientPublicKey: ClientPublicKey): Array[Byte] = serialize(clientPublicKey)
+  }
+
+  implicit val fromBytes: FromBytes[ClientPublicKey] = new FromBytes[ClientPublicKey] {
+    def apply(as: Array[Byte]): ClientPublicKey = deserialize[ClientPublicKey](as)
+  }
 }
 
 case class ServerPublicKey(value: Array[Byte]) extends PublicKey
@@ -29,4 +39,12 @@ object ServerPublicKey {
   implicit val encoder: Encoder[ServerPublicKey] = deriveEncoder
 
   implicit val decoder: Decoder[ServerPublicKey] = deriveDecoder
+
+  implicit val toBytes: ToBytes[ServerPublicKey] = new ToBytes[ServerPublicKey] {
+    def apply(serverPublicKey: ServerPublicKey): Array[Byte] = serialize(serverPublicKey)
+  }
+
+  implicit val fromBytes: FromBytes[ServerPublicKey] = new FromBytes[ServerPublicKey] {
+    def apply(as: Array[Byte]): ServerPublicKey = deserialize[ServerPublicKey](as)
+  }
 }
